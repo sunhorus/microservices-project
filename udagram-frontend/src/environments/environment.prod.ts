43485a4cdb1a086376d44dd6a5e1da2b88a0a129
Mostafa Said <sunhorus@gmail.com>
2021-11-22 19:20:57 +0200

    frontend commit

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a978805bedad5424da9db020fa48af12-836157187.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
